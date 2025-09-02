d41f7e7564d0dcf48838000d798cc9d3b0b413a0
Flow-Sub <f190240@nu.edu.pk>
2025-09-03 01:39:41 +0500

    feat: 3 - Forms

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'App',
+  description: 'Created by Magnetar',
   generator: 'v0.app',
 }
 
